0f8cb2f21062da7403bfc0ab5b978e1551e8c8f5
vyachgudzenko <vyach.gudzenko@gmail.com>
2022-05-19 20:13:37 +0300

    Initial Commit

---
+++ CollectViewWTF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectViewWTF
+//
+//  Created by Вячеслав Гудзенко on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
